634cddff67fe1b3659ad6f8ad4338bc9ca279e6e
thisari hettiarachchi <msthisari@gmail.com>
2025-06-12 20:38:46 +0530

    new

---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef, useState } from "react";
+import "../Projects/Projects.css";
+
+const Project = () => {
+
+  return (
+    <section className="projects" id="projects">
+        <h2 className="heading">
+            <i className="bx bx-code"></i> Projects
+        </h2>
+
+        <div className="project-container">
+        </div>
+    </section>
+  );
+};
+
+export default Projects;
